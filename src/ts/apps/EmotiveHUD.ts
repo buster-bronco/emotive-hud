6c8f9cb4e231cfa6becf4baa9deb7dcfb0b37f36
buster-bronco <105906709+reels-horse@users.noreply.github.com>
2024-12-27 17:36:53 -0800

    removed depricated mergeobject

--- src/ts/apps/EmotiveHUD.ts
+++ src/ts/apps/EmotiveHUD.ts
@@ -2,7 +2,7 @@ import { CONSTANTS } from "../constants";
 
 export default class EmotiveHUD extends Application {
     static override get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
+        return foundry.utils.mergeObject(super.defaultOptions, {
             id: `${CONSTANTS.MODULE_ID}-hud`,
             template: `modules/${CONSTANTS.MODULE_ID}/templates/emotive-hud.hbs`,
             popOut: false,
