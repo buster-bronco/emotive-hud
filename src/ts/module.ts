9f5e203a0c5cb5c395ce312d6068f1a3263fe5c9
buster-bronco <105906709+reels-horse@users.noreply.github.com>
2024-12-28 18:01:27 -0800

    removed template boilerplate

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,5 +1,4 @@
 import "../styles/style.scss";
-import DogBrowser from "./apps/dogBrowser";
 import EmotiveHUD from "./apps/EmotiveHUD";
 import { CONSTANTS } from "./constants";
 import { MyModule } from "./types";
@@ -14,20 +13,8 @@ Hooks.once("init", () => {
   console.log(`Initializing ${CONSTANTS.MODULE_ID}`);
 
   module = (game as Game).modules.get(CONSTANTS.MODULE_ID) as MyModule;
-  module.dogBrowser = new DogBrowser();
   module.emotiveActorSelector = new EmotiveActorSelector();
   module.emotiveHUD = new EmotiveHUD();
-});
-
-// Dog HUD from template. for sanity checking
-Hooks.on("renderActorDirectory", (_: Application, html: JQuery) => {
-  const button = $(
-    `<button class="cc-sidebar-button" type="button">🐶</button>`
-  );
-  button.on("click", () => {
-    module.dogBrowser.render(true);
-  });
-  html.find(".directory-header .action-buttons").append(button);
 });
 
 // EmotiveHUD hook
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,10 +1,8 @@
 import { ModuleData } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/packages.mjs";
-import DogBrowser from "./apps/dogBrowser";
 import EmotiveHUD from "./apps/EmotiveHUD";
 import EmotiveActorSelector from "./apps/EmotiveActorSelector";
 
 export interface MyModule extends Game.ModuleData<ModuleData> {
-  dogBrowser: DogBrowser;
   emotiveActorSelector: EmotiveActorSelector; 
   emotiveHUD: EmotiveHUD; 
 }
